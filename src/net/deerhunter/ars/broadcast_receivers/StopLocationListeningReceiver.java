f50a6332c6a1929c5f01007e217637e47837911d
vityokkv73 <vityokkv73@gmail.com>
2012-12-22 01:56:42 +0200

    deleted not necessary import
    Signed-off-by: vityokkv73 <vityokkv73@gmail.com>

--- src/net/deerhunter/ars/broadcast_receivers/StopLocationListeningReceiver.java
+++ src/net/deerhunter/ars/broadcast_receivers/StopLocationListeningReceiver.java
@@ -11,7 +11,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.Location;
 import android.location.LocationManager;
-import android.util.Log;
 
 public class StopLocationListeningReceiver extends BroadcastReceiver {
 
